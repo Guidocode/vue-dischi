794502d233dd7c5a3daebda1853921d2c7dfcfe6
Guidocode <guidobuono@icloud.com>
2022-05-18 13:27:51 +0200

    Aggiunta logica errore axios

--- src/components/MainComp.vue
+++ src/components/MainComp.vue
@@ -1,24 +1,32 @@
 <template>
   <main>
 
-    <div v-if="!isLoading"
-    class="gb-container container-cards p-5">
+    <!-- Logica errore -->
+    <div v-if="!isError" class="h-100">
+      <div v-if="!isLoading"
+      class="gb-container container-cards p-5">
 
-      <div class="container-fluid">
-        <div class="row row-cols-1 row-cols-sm-2 row-cols-md-3 row-cols-lg-4 row-cols-xl-5 d-flex">
+        <div class="container-fluid">
+          <div class="row row-cols-1 row-cols-sm-2 row-cols-md-3 row-cols-lg-4 row-cols-xl-5 d-flex">
 
-          <CardComp 
-          v-for="(coverItem, index) in arrayCovers" :key="`coverItem-${index}`"
-          :cover="coverItem"
-          />
+            <CardComp 
+            v-for="(coverItem, index) in arrayCovers" :key="`coverItem-${index}`"
+            :cover="coverItem"
+            />
+
+          </div>
 
         </div>
-
       </div>
+
+      <!-- Caricamento fino a che isLoading diventa false -->
+      <LoadingComp v-else />
+
     </div>
-
-    <LoadingComp v-else />
-
+    
+    <div v-else class="text-center my-5">{{errorMessage}}</div>
+    <!-- /Logica errore -->
+    
   </main>
 </template>
 
@@ -36,10 +44,12 @@ export default {
 
   data(){
     return{
-      baseUrl: 'https://flynn.boolean.careers/exercises/api/array/music',
+      baseUrl: 'https://flynn.boolean.careers/exercises/api/array/musicx',
 
       arrayCovers: [],
-      isLoading: true
+      isLoading: true,
+      errorMessage: '',
+      isError: false
     }
   },
 
@@ -50,6 +60,11 @@ export default {
         console.log('res', resp.data.response);
         this.arrayCovers = resp.data.response;
         this.isLoading = false;
+      })
+      .catch(error => {
+        this.errorMessage = error;
+        this.isError = true;
+        console.log('ERRORE', this.errorMessage);
       })
     }
   },
